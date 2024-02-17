e83e69085a2fddb37ee4ea10f5e85257e00e1877
Robert McCreary Mannino <fooyinfighter@gmail.com>
2024-02-17 10:20:31 -0800

    add js for button

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("it works!!!")
+console.log("it works!!!");
+
+myButton.onclick = function () {
+    let myDate = new Date();
+    let hours = myDate.getHours();
+    alert("the hour is: " + hours);
+}
